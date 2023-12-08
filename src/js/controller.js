9a48c815c0062d13b2e03250732d68e92746f272
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2023-12-07 20:59:33 -0800

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,35 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    // console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
